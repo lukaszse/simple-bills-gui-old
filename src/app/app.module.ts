20521ea8883b8e8a0f1f9bad1f3e9d91cbc61e67
llseremak@gmail.com <Amdk62Amd!>
2022-09-11 19:17:36 +0200

    Fix NG config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {FooterComponent} from './footer/footer.component';
+import {ContentComponent} from './content/content.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from './content/home/home.component';
+import {BillsComponent} from './content/bills/bills.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'bills', component: BillsComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    ContentComponent,
+    HomeComponent,
+    BillsComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
